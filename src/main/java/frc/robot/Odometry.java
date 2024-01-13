a70b7118ea64c900a3c35e3b87517f008740023e
Jadyn-Mithani <109890418+Jadyn-Mithani@users.noreply.github.com>
2024-01-12 17:53:22 -0800

    ..

--- src/main/java/frc/robot/Odometry.java
+++ src/main/java/frc/robot/Odometry.java
@@ -1,23 +1,21 @@
 package frc.robot;
-
-import java.sql.Driver;
-
-import com.ctre.phoenix6.Utils;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain.SwerveDriveState;
-
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.units.Units;
 
 
-public class Odometry {
+public class Odometry extends LimelightHelpers {
+
+    Vision Limelight1 = new Vision("/limelight/<botpose>");
+    String CamName = "limelight";
+    private SwerveDrivePoseEstimator VOdom = new SwerveDrivePoseEstimator(null, null, null, null, null, null);
+
+    public void UpdateVision() {
+        VOdom.addVisionMeasurement(Limelight1.getPos2D() , Limelight1.getLatestTimestamp());
+    }
+
+
     
 }
