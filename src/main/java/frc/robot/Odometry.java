f2dbcf0dbbc7852e5147384a2e8e51f855fe00d0
Jadyn-Mithani <109890418+Jadyn-Mithani@users.noreply.github.com>
2024-01-15 16:29:08 -0800

    Added Limelight helpers

--- src/main/java/frc/robot/Odometry.java
+++ src/main/java/frc/robot/Odometry.java
@@ -2,16 +2,17 @@ package frc.robot;
 
 // import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 
-public class Odometry {
+// public class Odometry {
 
-  Vision Limelight1 = new Vision("/limelight/<botpose>");
-  String CamName = "limelight";
-  private SwerveDrivePoseEstimator VOdom = new SwerveDrivePoseEstimator(null, null, null, null, null, null);
+// Vision Limelight1 = new Vision("/limelight/<botpose>");
+// String CamName = "limelight";
+// private SwerveDrivePoseEstimator VOdom = new SwerveDrivePoseEstimator(null, null, null, null,
+// null, null);
 
-  public void UpdateVisionOdom() {
+// public void UpdateVisionOdom() {
 
-    VOdom.addVisionMeasurement(Limelight1.getPos2D(), Limelight1.getLatestTimestamp());
+// VOdom.addVisionMeasurement(Limelight1.getPos2D(), Limelight1.getLatestTimestamp());
 
-  }
+// }
 
-}
+// }
