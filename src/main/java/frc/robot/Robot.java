287636509d0ea801f07723683002dcb22e7432cc
Jadyn-Mithani <109890418+Jadyn-Mithani@users.noreply.github.com>
2024-02-04 15:23:37 -0800

    Moved BuildConstants to generated folder

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.generated.BuildConstants;
 
 public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -1,4 +1,4 @@
-package frc.robot;
+package frc.robot.generated;
 
 /**
  * Automatically generated file containing build version information.
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "6364-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 134;
-  public static final String GIT_SHA = "a2250c13bf9e196f9b2de382b8053584945387f2";
-  public static final String GIT_DATE = "2024-02-04 14:40:43 PST";
+  public static final int GIT_REVISION = 135;
+  public static final String GIT_SHA = "929a4296a8abea93289ea37f5962d47495df1177";
+  public static final String GIT_DATE = "2024-02-04 15:17:17 PST";
   public static final String GIT_BRANCH = "feature/logger";
-  public static final String BUILD_DATE = "2024-02-04 15:15:53 PST";
-  public static final long BUILD_UNIX_TIME = 1707088553270L;
+  public static final String BUILD_DATE = "2024-02-04 15:22:14 PST";
+  public static final long BUILD_UNIX_TIME = 1707088934704L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
