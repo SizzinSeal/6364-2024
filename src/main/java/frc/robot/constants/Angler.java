c633ba8a8cfefd45267db87114156682bfb36530
TenTonFRC <jaron@cyna.com>
2024-03-01 10:26:19 -0800

    slow down angler, now with S-curve motion profiling

--- src/main/java/frc/robot/constants/Angler.java
+++ src/main/java/frc/robot/constants/Angler.java
@@ -29,15 +29,15 @@ public class Angler {
   public static final double kShootingPosition = 130; // TODO: find this value
   public static final double kLoadingPosition = 20; // TODO: find this value
   // speeds (in rotations per second)
-  public static final double kMaxSpeed = 150; // TODO: find this value
+  public static final double kMaxSpeed = 120; // TODO: find this value
   public static final double kManualSpeed = 30; // TODO: find this value
   public static final double kProbeSpeed = 30; // TODO: find this value
   // acceleration (in rotations per second squared)
-  public static final double kAcceleration = 400; // TODO: find this value
+  public static final double kAcceleration = 100; // TODO: find this value
   public static final double kManualAcceleration = 0; // TODO: find this value
   public static final double kProbeAcceleration = 0; // TODO: find this value
   // jerk (in rotations per second cubed)
-  public static final double kJerk = 0;
+  public static final double kJerk = 400;
   // tolerances (in rotations)
   public static final double kTolerance = 0.013889; // 5 degrees
   // ratios (driven/driver)
