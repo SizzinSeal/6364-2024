eadf91980626f47253c111f1f04f50fbb5341798
Jadyn Mithani <59635961+n-cho@users.noreply.github.com>
2024-03-14 15:15:48 -0700

    not working, finding values for Pose 2d calculation

--- src/main/java/frc/robot/PhotonVisionHandler.java
+++ src/main/java/frc/robot/PhotonVisionHandler.java
@@ -4,11 +4,16 @@ import java.util.List;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonTrackedTarget;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class PhotonVisionHandler {
   private static PhotonCamera vision = new PhotonCamera("photonvision");
   private static PhotonTrackedTarget target = vision.getLatestResult().getBestTarget();
+  private final static double kCameraHeight = 0.35052; // meters
+
+  // in order of ID 1 to ID 16
+  private static double[] AprilTagHeights = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
   /**
    * Get the number of April Tags in total
@@ -25,8 +30,10 @@ public class PhotonVisionHandler {
     return target.getArea(); // gets area of apriltag
   }
 
+  // camera dimensions from center of robot (x, y, z) (10.4x, -6.5y, 13.8z) inches
+  // front is intake (y), pos x to the right of that, z right
   public static Pose2d getPos2D() {
-    Pose2D robotPose = PhotonUtils.estimateFieldToRobot(kCameraHeight, kTargetHeight, kCameraPitch,
+    return PhotonUtils.estimateFieldToRobot(kCameraHeight, kTargetHeight, kCameraPitch,
         kTargetPitch, Rotation2d.fromDegrees(-target.getYaw()), gyro.getRotation2d(), targetPose,
         cameraToRobot);
   }
